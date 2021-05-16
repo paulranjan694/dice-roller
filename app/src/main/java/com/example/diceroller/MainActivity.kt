516f6f1548a294797ca437da61f8d17da22c6246
paulranjan694 <paulranjan694@gmail.com>
2021-05-17 01:33:01 +0530

    App done

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
-import kotlin.random.Random
+import android.widget.ImageView
+
+
 
 class MainActivity : AppCompatActivity() {
+    
+    lateinit var diceImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,11 +18,20 @@ class MainActivity : AppCompatActivity() {
             //Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
             rollDice()
         }
+       diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
         val randomInt =(1..6).random()
-        resultText.text = randomInt.toString()
+       
+        val drawableResourse = when(randomInt){
+            1->R.drawable.dice_1
+            2->R.drawable.dice_2
+            3->R.drawable.dice_3
+            4->R.drawable.dice_4
+            5->R.drawable.dice_5
+            else->R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResourse)
     }
 }
